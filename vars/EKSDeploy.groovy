fc57bcd3a20d74dd4d58889dd878a3eb2ee2a10f
kishorep-08 <kishore.pkdp@gmail.com>
2026-02-05 21:54:56 +0530

    jenkins

--- vars/EKSDeploy.groovy
+++ vars/EKSDeploy.groovy
@@ -20,10 +20,11 @@ def call(Map configMap) {
                     script {
                         withAWS(region:'us-east-1',credentials:'aws-auth') {
                             sh """
-                                echo "Triggering Deploy Job ..."
                                 echo "Deploying application version ${appVersion} to ${deploy_to} environment..."
                                 aws eks update-kubeconfig --region ${REGION} --name ${PROJECT}-${deploy_to}
                                 kubectl get nodes
+                                sed -i "s/IMAGE_VERSION/${appVersion}/g" values.yaml
+                                helm upgrade --install ${COMPONENT} -f values-${deploy_to}.yaml -n ${PROJECT} --atomic --wait --timeout=5m .
                             """
                         }                
                     }
